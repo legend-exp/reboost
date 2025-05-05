fa675f74f3b9cba7825a3ea481654c08a6c873fe
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2025-05-05 17:13:43 +0200

    fix tests

--- tests/hpge/test_hpge_map.py
+++ tests/hpge/test_hpge_map.py
@@ -12,16 +12,16 @@ from reboost.utils import u
 def test_read_hpge_map():
     dt_map = get_hpge_scalar_rz_field(
         f"{Path(__file__).parent}/simulation/drift-time-maps.lh5",
-        "V99999A",
+        "V99000A",
         "drift_time",
         out_of_bounds_val=0,
     )
 
     assert isinstance(dt_map, HPGeScalarRZField)
 
-    assert dt_map.r_units == u["m"]
-    assert dt_map.z_units == u["m"]
-    assert dt_map.φ_units == u["ns"]
+    assert dt_map.r_units == u.m
+    assert dt_map.z_units == u.m
+    assert dt_map.φ_units == u.ns
 
     assert isinstance(dt_map.φ, RegularGridInterpolator)
 
@@ -29,4 +29,4 @@ def test_read_hpge_map():
         dt_map.φ((0, -1))
 
     assert dt_map.φ((0, 0)) == 0
-    assert dt_map.φ([(0, 0.001), (0.005, 0.005)]) == pytest.approx([393.0189, 105.1679])
+    assert dt_map.φ([(0, 0.01), (0.03, 0.03)]) == pytest.approx([135, 695])
