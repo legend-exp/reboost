0d409ff3597be9d6d60042fc397c462c7c999e25
Manuel Huber <info@manuelhu.de>
2025-12-04 12:39:45 +0100

    add load_optmap_all to __all__

--- src/reboost/spms/__init__.py
+++ src/reboost/spms/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
-from .pe import detected_photoelectrons, emitted_scintillation_photons, load_optmap
+from .pe import detected_photoelectrons, emitted_scintillation_photons, load_optmap, load_optmap_all
 
-__all__ = ["detected_photoelectrons", "emitted_scintillation_photons", "load_optmap"]
+__all__ = [
+    "detected_photoelectrons",
+    "emitted_scintillation_photons",
+    "load_optmap",
+    "load_optmap_all",
+]
