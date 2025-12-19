05360fd6ed05750b9234e741bbe259e432e77922
Manuel Huber <ManuelHu@users.noreply.github.com>
2025-12-19 18:35:20 +0100

    docs: add geant4 macro lexer

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,10 @@ from __future__ import annotations
 import importlib.metadata
 import sys
 from pathlib import Path
+from typing import ClassVar
 
+from pygments.lexer import RegexLexer
+from pygments.token import Comment, Name, Text
 from sphinx.ext.napoleon.docstring import GoogleDocstring, NumpyDocstring
 
 sys.path.insert(0, Path(__file__).parents[2].resolve().as_posix())
@@ -79,3 +82,33 @@ autodoc_type_aliases = {
     "ArrayLike": "ArrayLike",
     "NDArray": "NDArray",
 }
+
+
+def _replace(app, docname, source):  # noqa: ARG001
+    result = source[0]
+    for key in app.config.ultimate_replacements:
+        result = result.replace(key, app.config.ultimate_replacements[key])
+    source[0] = result
+
+
+class Geant4MacroLexer(RegexLexer):
+    name: str = "Geant4Macro"
+    aliases: ClassVar[list[str]] = ["geant4", "g4mac"]
+    filenames: ClassVar[list[str]] = ["*.mac"]
+
+    tokens: ClassVar[dict] = {
+        "root": [
+            # comments
+            (r"#.*$", Comment),
+            # command name at start of line
+            (r"^(/\S+)", Name.Function),
+            # geant4 alias
+            (r"\{\w+\}", Name.Variable),
+            # any other text
+            (r".", Text),
+        ],
+    }
+
+
+def setup(app):
+    app.add_lexer("geant4", Geant4MacroLexer)
