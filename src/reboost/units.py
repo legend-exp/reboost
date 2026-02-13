e24773e2f12d146898c11603cbc19c5c06c5d81a
Manuel Huber <ManuelHu@users.noreply.github.com>
2026-02-13 12:23:49 +0100

    units: revert some unintended changes (#170)
    * units: revert some unintended changes

--- src/reboost/units.py
+++ src/reboost/units.py
@@ -4,7 +4,6 @@ import logging
 from typing import Any
 
 import awkward as ak
-import numpy as np
 import pint
 import pyg4ometry as pg4
 from lgdo import LGDO
@@ -115,12 +114,9 @@ def units_conv_ak(
     # return ak.Array
     if isinstance(data, LGDO):
         return data.view_as("ak")
-
-    if isinstance(data, np.ndarray):
-        return ak.Array(data)
-    if data is not None:
-        return ak.Array(data)
-    return data
+    if isinstance(data, ak.Array):
+        return data
+    return ak.Array(data)
 
 
 def unwrap_lgdo(data: Any | LGDO | ak.Array, library: str = "ak") -> tuple[Any, pint.Unit | None]:
