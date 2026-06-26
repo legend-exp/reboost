c8d47812402100e4d28bfd0bc4c8d88486b6bc2d
Manuel Huber <info@manuelhu.de>
2026-06-26 12:01:23 +0200

    patch old particle charge function back in

--- src/reboost/optmap/numba_pdg.py
+++ src/reboost/optmap/numba_pdg.py
@@ -8,15 +8,35 @@ from numba import njit
 
 def numba_pdgid_funcs():
     """Load a numby-optimized copy of the scikit-hep/particle package."""
-    spec_pdg = importlib.util.find_spec("particle.pdgid.functions")
-    pdg_func = importlib.util.module_from_spec(spec_pdg)
-    spec_pdg.loader.exec_module(pdg_func)
+
+    def _import_module(mod: str):
+        spec = importlib.util.find_spec(mod)
+        assert spec is not None
+        modobj = importlib.util.module_from_spec(spec)
+        assert spec.loader is not None
+        spec.loader.exec_module(modobj)
+        return modobj
+
+    pdg_func = _import_module("particle.pdgid.functions")
+    pdg_version = _import_module("particle.version")
 
     def _digit2(pdgid, loc: int) -> int:
         e = 10 ** (loc - 1)
         return (pdgid // e % 10) if pdgid >= e else 0
 
+    # version from particle < 1
+    def charge2(pdgid) -> float | None:
+        """Returns the charge."""
+        three_charge_pdgid = pdg_func.three_charge(pdgid)
+        if three_charge_pdgid is None:
+            return None
+        if not pdg_func.is_Qball(pdgid):
+            return three_charge_pdgid / 3.0
+        return three_charge_pdgid / 30.0
+
     pdg_func._digit = _digit2
+    if pdg_version.version_tuple[0] >= 1:
+        pdg_func.charge = charge2
 
     for fname, f in pdg_func.__dict__.items():
         if not callable(f) or not isinstance(f, types.FunctionType):
